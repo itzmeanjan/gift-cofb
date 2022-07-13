c1ec74374fb9d5ca703f0a3da97bc5877100fa3f
Anjan Roy <hello@itzmeanjan.in>
2022-07-13 10:17:02 +0400

    substitute cells step of round function
    Signed-off-by: Anjan Roy <hello@itzmeanjan.in>

--- include/gift.hpp
+++ include/gift.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 
@@ -18,7 +19,7 @@ struct state_t
 // key, as defined in section 2.4.2 of GIFT-COFB specification
 // https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/gift-cofb-spec-final.pdf
 inline static void
-initialize(state_t* const st,                   // GIFT-128 block cipher state
+initialize(state_t* const __restrict st,        // GIFT-128 block cipher state
            const uint8_t* const __restrict txt, // 128 -bit plain text block
            const uint8_t* const __restrict key  // 128 -bit secret key
 )
@@ -38,6 +39,31 @@ initialize(state_t* const st,                   // GIFT-128 block cipher state
     st->key[i] = (static_cast<uint16_t>(key[boff ^ 0]) << 8) |
                  (static_cast<uint16_t>(key[boff ^ 1]) << 0);
   }
+}
+
+// Substitutes cells of cipher state with following instructions, as defined in
+// page 5 of GIFT-COFB specification
+// https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/gift-cofb-spec-final.pdf
+inline static void
+subcells(state_t* const st)
+{
+  const uint32_t t0 = st->cipher[0] & st->cipher[2];
+  st->cipher[1] ^= t0;
+
+  const uint32_t t1 = st->cipher[1] & st->cipher[3];
+  st->cipher[0] ^= t1;
+
+  const uint32_t t2 = st->cipher[0] | st->cipher[1];
+  st->cipher[2] ^= t2;
+
+  st->cipher[3] ^= st->cipher[2];
+  st->cipher[1] ^= st->cipher[3];
+  st->cipher[3] = ~st->cipher[3];
+
+  const uint32_t t3 = st->cipher[0] & st->cipher[1];
+  st->cipher[2] ^= t3;
+
+  std::swap(st->cipher[0], st->cipher[3]);
 }
 
 }
